3eb2762d93cc193b104da740e0c1be934c645892
Deeti <deetigupta8@gmail.com>
2023-03-10 14:32:20 +0530

    basic setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  return (
+    <h2>Weather App</h2>
+  )
+}
+
+export default App
